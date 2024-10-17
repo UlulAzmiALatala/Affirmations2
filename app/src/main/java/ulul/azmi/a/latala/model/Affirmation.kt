e783f450ee398f3a4c359c30d90dfc1917e1fdc9
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-10-18 00:07:16 +0700

    Update Affirmation.kt

--- app/src/main/java/ulul/azmi/a/latala/model/Affirmation.kt
+++ app/src/main/java/ulul/azmi/a/latala/model/Affirmation.kt
@@ -3,7 +3,8 @@ package ulul.azmi.a.latala.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
+// Data class yang merepresentasikan sebuah afirmasi
 data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
+    @StringRes val stringResourceId: Int, // ID resource string untuk teks afirmasi
+    @DrawableRes val imageResourceId: Int // ID resource drawable untuk gambar afirmasi
 )
